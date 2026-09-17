536f89d0588a5d7167714c4cc60fa8a1e977272e
Tabbbb <tabareapecet@gmail.com>
2026-09-17 20:55:13 -0300

    ok

---
+++ Grupo1/SPLASH.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SPLASH
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Label1 = New Label()
+        ProgressBar1 = New ProgressBar()
+        Timer1 = New Timer(components)
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Tw Cen MT Condensed", 48F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(82, 80)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(220, 74)
+        Label1.TabIndex = 0
+        Label1.Text = "Level Up"
+        ' 
+        ' ProgressBar1
+        ' 
+        ProgressBar1.Location = New Point(118, 196)
+        ProgressBar1.Name = "ProgressBar1"
+        ProgressBar1.Size = New Size(151, 23)
+        ProgressBar1.TabIndex = 1
+        ' 
+        ' Timer1
+        ' 
+        ' 
+        ' SPLASH
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(398, 280)
+        Controls.Add(ProgressBar1)
+        Controls.Add(Label1)
+        FormBorderStyle = FormBorderStyle.None
+        Name = "SPLASH"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "SPLASH"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Grupo1/SPLASH.vb
@@ -0,0 +1,15 @@
+﻿Public Class SPLASH
+    Private Sub SPLASH_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+        Do While ProgressBar1.Value < 100
+            Application.DoEvents()
+        Loop
+        Timer1.Enabled = False
+        ProgressBar1.Value = 100
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ProgressBar1.Value = +1
+    End Sub
+End Class
