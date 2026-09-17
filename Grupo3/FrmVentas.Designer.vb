ede25a27c550b14686d52113dd77c34c76bb0f19
Nazaa-N <nietonazareno9@gmail.com>
2026-09-17 20:57:01 -0300

    Ventas

---
+++ Grupo3/FrmVentas.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmVentas
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(463, 151)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(254, 144)
+        Button1.TabIndex = 0
+        Button1.Text = "Salir"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' FrmVentas
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1265, 490)
+        Controls.Add(Button1)
+        Name = "FrmVentas"
+        Text = "FrmVentas"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+End Class
