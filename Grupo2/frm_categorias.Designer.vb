26601efa7aa94610e6deeb4b281959cec106adf1
SuperPocho2013 <superpocho2013@gmail.com>
2026-09-17 20:58:34 -0300

    elpepe y etesech

--- Grupo2/frm_categorias.Designer.vb
+++ Grupo2/frm_categorias.Designer.vb
@@ -22,15 +22,30 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Label1 = New Label()
         SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 100.0F)
+        Label1.Location = New Point(141, 123)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(489, 177)
+        Label1.TabIndex = 0
+        Label1.Text = "kahoot"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Label1)
         Name = "Form1"
         Text = "categorias"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Label1 As Label
 End Class
