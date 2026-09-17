291f6b3720a4c8031d98745bc260f731cda5bd13
Tabbbb <tabareapecet@gmail.com>
2026-09-17 20:59:00 -0300

    ok

--- Grupo1/SPLASH.vb
+++ Grupo1/SPLASH.vb
@@ -5,11 +5,11 @@
             Application.DoEvents()
         Loop
         Timer1.Enabled = False
-        ProgressBar1.Value = 100
+
 
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        ProgressBar1.Value = +1
+        ProgressBar1.Value += 1
     End Sub
 End Class
