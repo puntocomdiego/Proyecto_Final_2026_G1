f95377c226b96b14469eb31af47ebbbf06bc1f7b
Alex <nobilebrian9@gmail.com>
2026-09-17 20:59:51 -0300

    Actualizacion Boton salir

---
+++ Grupo3/Frm_Consultas_De_ventas.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_Consultas_De_ventas
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 20F)
+        Button1.Location = New Point(486, 323)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(174, 70)
+        Button1.TabIndex = 0
+        Button1.Text = "Exit"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Frm_Consultas_De_ventas
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(672, 405)
+        Controls.Add(Button1)
+        Name = "Frm_Consultas_De_ventas"
+        Text = "Frm_Consultas_De_ventas"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Grupo3/Frm_Consultas_De_ventas.vb
@@ -0,0 +1,14 @@
+﻿Public Class Frm_Consultas_De_ventas
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim respuesta As DialogResult
+        respuesta = MsgBox("¿Desea salir de la consulta de ventas?", MsgBoxStyle.Question + MsgBoxStyle.YesNo, "Salir")
+        If respuesta = DialogResult.Yes Then
+            Me.Close()
+        Else
+
+        End If
+
+    End Sub
+
+End Class
